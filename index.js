cb424d127fc46f14204a3cd286c26efc4261ed87
SysAdmin <123937984+rootHat-Admin@users.noreply.github.com>
2026-06-10 17:00:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const { Telegraf, Markup } = require('telegraf');
 const bot = new Telegraf('8996142007:AAHx1NhnirVOBHo-xP2GTGoz5CuzJl5mrDY');
 
 bot.start((ctx) => {
-  ctx.reply(
-    'Что там ДИ 👋 Пашалим?!',
-    Markup.inlineKeyboard([
-      Markup.button.webApp(
-        'GO BRO!',
-        'https://roothat-admin.github.io/good_exam/'
-      )
-    ])
+  ctx.replyWithAnimation(
+    'https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif',
+    {
+      caption: 'Что там ДИ 👋 Пашалим?!',
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Жми на меня!',
+              web_app: {
+                url: 'https://roothat-admin.github.io/good_exam/'
+              }
+            }
+          ]
+        ]
+      }
+    }
   );
 });
 
