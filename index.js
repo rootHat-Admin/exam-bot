b23a94c33ef4e363de2e793f9e82aa477f879801
SysAdmin <123937984+rootHat-Admin@users.noreply.github.com>
2026-06-10 17:17:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+const express = require('express');
 const { Telegraf, Markup } = require('telegraf');
 
-const bot = new Telegraf('8996142007:AAHx1NhnirVOBHo-xP2GTGoz5CuzJl5mrDY');
+const app = express();
 
+// Render health check
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+// Токен берём из ENV (на Render добавь BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// START с GIF + кнопкой
 bot.start((ctx) => {
   ctx.replyWithAnimation(
-    { source: './omaygad-mellstroy.gif' }, // 👈 ВАЖНО
+    { source: './omaygad-mellstroy.gif' }, // файл должен быть в репо
     {
       caption: 'Что там ДИ 👋 Пашалим?!',
       reply_markup: {
         inline_keyboard: [
           [
             {
-              text: 'Жми на меня!',
+              text: 'Жми на меня 🚀',
               web_app: {
-                url: 'https://roothat-admin.github.io/exam-bot/'
+                url: 'https://roothat-admin.github.io/good_exam/'
               }
             }
           ]
@@ -23,5 +33,10 @@ bot.start((ctx) => {
   );
 });
 
+// запускаем бот
 bot.launch();
 console.log('Bot started');
+
+// Render порт (ВАЖНО)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on', PORT));
