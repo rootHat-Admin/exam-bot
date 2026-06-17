50a0972a1dccabc0384fb895534fff77c1d19124
SysAdmin <123937984+rootHat-Admin@users.noreply.github.com>
2026-06-17 17:16:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { Telegraf, Markup } = require('telegraf');
 
 const app = express();
 
+// --- ВАШИ НАСТРОЙКИ ---
+const ADMIN_ID = 1029249076; 
+const MY_TG_LINK = 'https://t.me/t0n1gh1'; 
+// ----------------------
+
 // Render health check
 app.get('/', (req, res) => {
   res.send('Bot is running');
@@ -11,26 +16,32 @@ app.get('/', (req, res) => {
 // Токен берём из ENV (на Render добавь BOT_TOKEN)
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// START с GIF + кнопкой
+// START с проверкой доступа
 bot.start((ctx) => {
-  ctx.replyWithAnimation(
-    { source: './english.jpg' },
-    {
-      caption: 'Working Bro WORDS!?!',
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: 'Жми на меня 🚀',
-              web_app: {
-                url: 'https://effervescent-eclair-dbd8bc.netlify.app/'
+  if (ctx.from.id === ADMIN_ID) {
+    // ВАШ ДОСТУП
+    ctx.replyWithAnimation(
+      { source: './english.jpg' },
+      {
+        caption: 'Working Bro WORDS!?!',
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Click on me BRO!🚀',
+                web_app: {
+                  url: 'https://effervescent-eclair-dbd8bc.netlify.app/'
+                }
               }
-            }
+            ]
           ]
-        ]
+        }
       }
-    }
-  );
+    );
+  } else {
+    // ДОСТУП ДЛЯ ВСЕХ ОСТАЛЬНЫХ
+    ctx.reply(`ЭЭЭ, бро ты кто? Иди вон, тебе доступа нету! Доступ за 10 тыс долларов, если что пиши этому человеку: ${MY_TG_LINK}`);
+  }
 });
 
 // запускаем бот
